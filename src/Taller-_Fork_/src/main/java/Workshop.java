3d66431bd76e9631b0f4546a48fee11f5f5adf77
GitMinarai <sdsanchezt@udistrital.edu.co>
2026-09-14 13:55:24 -0500

    Resuelto elemento mayor con arreglo

--- src/Taller-_Fork_/src/main/java/Workshop.java
+++ src/Taller-_Fork_/src/main/java/Workshop.java
@@ -106,7 +106,13 @@ public class Workshop {
     public int encontrarElementoMayor(int[] arreglo) {
         // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
-        return 0;
+        int mayor = arreglo[0];
+        int i = 1;
+        while (i < arreglo.length){
+            if (arreglo[i] > mayor){
+                mayor = arreglo[i];}
+            i++;}
+        return mayor;
     }
 
     // Método que encuentra el elemento menor en un arreglo
