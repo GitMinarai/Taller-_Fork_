eca0a5b2b063c6d4cf6510d54ed627b2e671891b
GitMinarai <sdsanchezt@udistrital.edu.co>
2026-09-14 13:09:51 -0500

    Resuelto el verificador de números primos

--- src/Taller-_Fork_/src/main/java/Workshop.java
+++ src/Taller-_Fork_/src/main/java/Workshop.java
@@ -50,7 +50,13 @@ public class Workshop {
     public boolean esPrimo(int numero) {
         // TODO: Implementar el método para verificar si un número es primo.
         // Ejemplo: Si numero = 7, el resultado debería ser true.
-        return false;
+        if (numero < 2){
+            return false;}
+        for (int i = 2; i < numero; i++){
+            if ((numero / i) * i == numero){
+                return false;}
+        }
+        return true;
     }
 
     // Método que genera una serie de Fibonacci
