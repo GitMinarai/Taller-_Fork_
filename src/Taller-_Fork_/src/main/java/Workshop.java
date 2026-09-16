71182d8e7dfdad707b9128ba01fc18659cd8c712
GitMinarai <sdsanchezt@udistrital.edu.co>
2026-09-16 14:06:18 -0500

    Resuelto binario.

--- src/Taller-_Fork_/src/main/java/Workshop.java
+++ src/Taller-_Fork_/src/main/java/Workshop.java
@@ -390,7 +390,22 @@ public class Workshop {
     public String convertirABinario(int numero) {
         // TODO: Implementar el método para convertir un número en su representación binaria.
         // Ejemplo: Si numero = 10, el resultado debería ser "1010".
-        return "";
+        if (numero == 0){
+            return "0";}
+        if (numero < 0){
+            numero = numero * -1;
+            String respuesta = "";
+            while (numero > 0){
+                int residuo = numero - (numero / 2) * 2;
+                respuesta = residuo + respuesta;
+                numero = numero / 2;}
+            return "-" + respuesta;}
+        String resultado = "";
+        while (numero > 0){
+            int residuo = numero - (numero / 2) * 2;
+            resultado = residuo + resultado;
+            numero = numero / 2;}
+        return resultado;
     }
 
     // Método que convierte un número en su representación hexadecimal
